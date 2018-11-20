45be631bfb3e38dda98c4a192ceb1414249cf4cb
Nathan Manske <nathan.manske@utexas.edu>
2018-11-19 23:02:09 -0600

    Add first 2 problems from ctci ch 1

---
+++ ctci/ch01-arrays-and-strings/01-is-unique.py
@@ -0,0 +1,58 @@
+'''Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
+
+Questions to ask: Are we assuming ASCII strings? Extended ASCII? Unicode?
+'''
+
+import unittest
+
+# Runtime: O(n)
+def isUniqueDict(s):
+    # Assuming char set is standard ASCII
+    if len(s) > 128:
+        return False
+
+    chars = {}
+    for c in s:
+        if c in chars:
+            return False
+        else:
+            chars[c] = 1
+
+    return True
+
+# Runtime: O(n log(n))
+def isUniqueStr(s):
+    # Assuming char set is standard ASCII
+    if len(s) > 128:
+        return False
+
+    s = ''.join(sorted(s))
+    for i in range(0, len(s) - 1):
+        if s[i] == s[i+1]:
+            return False
+
+    return True
+
+class Test(unittest.TestCase):
+    dataT = (('abcdefg'), ('8fna0('), (''))
+    dataF = (('2fjzn2'), ('hfa 843 ( ('))
+
+    def test_unique_hash(self):
+        for test_string in self.dataT:
+            result = isUniqueDict(test_string)
+            self.assertTrue(result)
+        for test_string in self.dataF:
+            result = isUniqueDict(test_string)
+            self.assertFalse(result)
+
+    def test_unique_str(self):
+        for test_string in self.dataT:
+            result = isUniqueStr(test_string)
+            self.assertTrue(result)
+        for test_string in self.dataF:
+            result = isUniqueStr(test_string)
+            self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ctci/ch01-arrays-and-strings/02-check-permutation.py
@@ -0,0 +1,63 @@
+'''Given two strings, write a method to decide if one is a permutation
+of the other.
+
+Questions to ask: Is the comparison case sensitive? What about whitespace?
+'''
+
+import unittest
+
+def isPermutationSort(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    s1_sorted = ''.join(sorted(s1))
+    s2_sorted = ''.join(sorted(s2))
+
+    return s1_sorted == s2_sorted
+
+def isPermutationCounter(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    chars = {}
+
+    for c in s1:
+        chars[c] = chars.get(c, 0) + 1
+
+    for c in s2:
+        if chars.get(c, 0) == 0:
+            return False
+        chars[c] -= 1
+
+    return True
+
+class Test(unittest.TestCase):
+    dataT = (
+        ('abcd', 'bacd'),
+        ('3563476', '7334566'),
+        ('wef34f', 'wffe34'),
+    )
+    dataF = (
+        ('abcd', 'd2cba'),
+        ('2354', '1234'),
+        ('dcw4f', 'dcw5f'),
+    )
+
+    def test_permutation_sort(self):
+        for test_strings in self.dataT:
+            result = isPermutationSort(*test_strings)
+            self.assertTrue(result)
+        for test_strings in self.dataF:
+            result = isPermutationSort(*test_strings)
+            self.assertFalse(result)
+
+    def test_permutation_counter(self):
+        for test_strings in self.dataT:
+            result = isPermutationCounter(*test_strings)
+            self.assertTrue(result)
+        for test_strings in self.dataF:
+            result = isPermutationCounter(*test_strings)
+            self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
