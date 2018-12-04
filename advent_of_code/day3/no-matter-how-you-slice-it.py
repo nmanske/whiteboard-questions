91883ddcebf3c94ff94810d5a685a8b599429d14
Nathan Manske <nathan.manske@utexas.edu>
2018-12-03 22:36:31 -0600

    Complete aoc18 day3

--- advent_of_code/day3/no-matter-how-you-slice-it.py
+++ advent_of_code/day3/no-matter-how-you-slice-it.py
@@ -1,31 +1,50 @@
 import os
 
-def get_num_conflicts(file):
-    fabric = [[0 for col in range(1000)] for row in range(1000)]
+FABRIC_WIDTH = 1000
+FABRIC_HEIGHT = 1000
 
-    print(fabric[:][2])
+def get_fabric_state(file):
+    fabric = [[[] for col in range(FABRIC_WIDTH)] for row in range(FABRIC_HEIGHT)]
+    conflict_ids = []
 
     with open(file, 'r') as f:
         lines = f.read().strip().splitlines()
 
     for claim in lines:
-        claim = claim.split('@ ')[1].split(': ')
-        x_pos, y_pos = [int(c) for c in claim[0].split(',')]
-        width, height = [int(c) for c in claim[1].split('x')]
+        claim_id, location_info = claim.split('@ ')
+        claim_id = claim_id[1:]
+        location_info = location_info.split(': ')
+        x_pos, y_pos = [int(c) for c in location_info[0].split(',')]
+        width, height = [int(c) for c in location_info[1].split('x')]
         for i in range(x_pos, x_pos+width):
             for j in range(y_pos, y_pos+height):
-                fabric[j][i] += 1
+                fabric[j][i].append(claim_id)
+                if len(fabric[j][i]) > 1:
+                    for entry in fabric[j][i]:
+                        if entry not in conflict_ids:
+                            conflict_ids.append(entry)
 
+    return fabric, conflict_ids
+
+def get_num_conflicts(fabric):
     num_conflicts = 0
-    for i in range(1000):
-        for j in range(1000):
-            if fabric[j][i] >= 2:
+    for i in range(FABRIC_WIDTH):
+        for j in range(FABRIC_HEIGHT):
+            if len(fabric[j][i]) > 1:
                 num_conflicts += 1
-
     return num_conflicts
+
+def get_no_conflict_id(fabric, conflict_ids):
+    for i in range(FABRIC_WIDTH):
+        for j in range(FABRIC_HEIGHT):
+            if len(fabric[j][i]) == 1 and fabric[j][i][0] not in conflict_ids:
+                return fabric[j][i][0]
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(dir_path, 'day3.txt')
 
-num_conflicts = get_num_conflicts(input_file)
+fabric, conflict_ids = get_fabric_state(input_file)
+num_conflicts = get_num_conflicts(fabric)
+no_conflict_id = get_no_conflict_id(fabric, conflict_ids)
 print(num_conflicts)
+print(no_conflict_id)
