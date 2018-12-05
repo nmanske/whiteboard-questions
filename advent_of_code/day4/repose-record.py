e2f4f73301618109f3cf480c58b7e4d0bbe12001
Nathan Manske <nathan.manske@utexas.edu>
2018-12-04 19:52:30 -0600

    aoc18 day4 write sorted logs to output file

--- advent_of_code/day4/repose-record.py
+++ advent_of_code/day4/repose-record.py
@@ -1,15 +1,29 @@
 import os
+import re
 
-def get_sorted_logs(file):
-    with open(file, 'r+') as f:
+def get_sorted_logs(input_file, output_file):
+    with open(input_file, 'r+') as f:
         lines = f.readlines()
-        return lines.sort()
+        lines.sort().
 
-def get_vulnerable_guard_info(logs):
+    sorted_logs = []
+    with open(output_file, 'w') as f:
+        for line in lines:
+            line = re.sub(r'([\[\]])', '', line)
+            sorted_logs.append(line)
+            f.write(line)
+
+    return sorted_logs
+
+def get_lazy_guard_info(logs):
+    for log in logs:
+        pass
+
     return None
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(os.path.join(dir_path, 'day4.txt'))
+output_file = os.path.join(os.path.join(dir_path, 'day4_sorted.txt'))
 
-sorted_logs = get_sorted_logs(input_file)
-guard_id, minutes_asleep = get_vulnerable_guard_info(sorted_logs)
+sorted_logs = get_sorted_logs(input_file, output_file)
+guard_id, most_slept_minute = get_lazy_guard_info(sorted_logs)
