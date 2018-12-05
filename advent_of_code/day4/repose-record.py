3036886ec2fa5f6a2b97b6adc43cebcf282cb757
Nathan Manske <nathan.manske@utexas.edu>
2018-12-04 19:23:42 -0600

    Add file sorting method to aoc18 day4

--- advent_of_code/day4/repose-record.py
+++ advent_of_code/day4/repose-record.py
@@ -1,6 +1,15 @@
 import os
 
+def get_sorted_logs(file):
+    with open(file, 'r+') as f:
+        lines = f.readlines()
+        return lines.sort()
 
+def get_vulnerable_guard_info(logs):
+    return None
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(os.path.join(dir_path, 'day4.txt'))
+
+sorted_logs = get_sorted_logs(input_file)
+guard_id, minutes_asleep = get_vulnerable_guard_info(sorted_logs)
