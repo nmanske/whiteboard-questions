86fc0654fe847fd00b82646a47764700869ed84a
Nathan Manske <nathan.manske@utexas.edu>
2018-12-08 18:26:59 -0600

    [aoc18 day5] Complete part 2

--- advent_of_code/day5/alchemical-reduction.py
+++ advent_of_code/day5/alchemical-reduction.py
@@ -1,26 +1,56 @@
 import os
+import re
 
-def get_reduction(file):
-    with open(file, 'r') as f:
-        formula = f.readline().strip()
+UPPERCASE_A = 65 # ASCII decimal
+NUM_LETTERS = 26
 
+def get_reduction(polymer):
     is_reaction = True
     while(is_reaction):
         is_reaction = False
-        for i in range(0, len(formula)-1):
-            if ((formula[i].isupper() and formula[i+1].islower()) or \
-                (formula[i].islower() and formula[i+1].isupper())) and \
-                formula[i].upper() == formula[i+1].upper():
-                formula = formula[:i] + formula[i+2:]
+        for i in range(0, len(polymer)-1):
+            if ((polymer[i].isupper() and polymer[i+1].islower()) or \
+                (polymer[i].islower() and polymer[i+1].isupper())) and \
+                polymer[i].upper() == polymer[i+1].upper():
+                polymer = polymer[:i] + polymer[i+2:]
                 is_reaction = True
                 break
 
-    return formula
+    return polymer
+
+def get_improved_reduction(polymer):
+    shortest_polymer = None
+
+    for i in range(NUM_LETTERS):
+        letter_code = hex(UPPERCASE_A + i).replace('0x', '')
+        pattern = r'([\x' + letter_code + r'])+'
+        modified_polymer = re.sub(pattern, '', polymer, flags=re.IGNORECASE)
+        reduced_modified_polymer = get_reduction(modified_polymer)
+        if shortest_polymer is None or len(reduced_modified_polymer) < len(shortest_polymer):
+            shortest_polymer = reduced_modified_polymer
+
+    return shortest_polymer
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
+
 input_file = os.path.join(dir_path, 'day5.txt')
+with open(input_file, 'r') as f:
+    polymer = f.readline().strip()
 
 # Part 1
 
-reduction = get_reduction(input_file)
-print(reduction)
+reduction = get_reduction(polymer)
+print('Reduction length: ' + str(len(reduction)))
+
+reduction_file = os.path.join(os.path.join(dir_path, 'day5_reduction.txt'))
+with open(reduction_file, 'w') as f:
+    f.write(reduction)
+
+# Part 2
+
+improved_reduction = get_improved_reduction(polymer)
+print('Improved reduction length: ' + str(len(improved_reduction)))
+
+improved_reduction_file = os.path.join(os.path.join(dir_path, 'day5_improved_reduction.txt'))
+with open(improved_reduction_file, 'w') as f:
+    f.write(improved_reduction)
